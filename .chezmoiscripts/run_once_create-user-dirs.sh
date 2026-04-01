089bc71c482138c69ef948ec53e248d4f96e1ea5
lenitain <xt.zhu@qq.com>
2026-04-01 10:29:26 +0800

    create user-dirs script

--- .chezmoiscripts/run_once_create-user-dirs.sh
+++ .chezmoiscripts/run_once_create-user-dirs.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p ~/user-dirs
+mkdir -p ~/Desktop
+mkdir -p ~/Downloads
+mkdir -p ~/Templates
+mkdir -p ~/Public
+mkdir -p ~/Documents
+mkdir -p ~/Music
+mkdir -p ~/Pictures
+mkdir -p ~/Videos
