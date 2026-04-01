99242010e6e0996dc78d49760602392c434d3039
lenitain <xt.zhu@qq.com>
2026-04-01 10:47:34 +0800

    add more pacman install content

--- .chezmoiscripts/run_once_after_install-packages.sh
+++ .chezmoiscripts/run_once_after_install-packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-if command -v pacman &> /dev/null; then
-    sudo pacman -S --needed --noconfirm neovim
-elif command -v apt &> /dev/null; then
-    sudo apt install -y neovim
-elif command -v dnf &> /dev/null; then
-    sudo dnf install -y neovim
+if command -v pacman &>/dev/null; then
+  sudo pacman -S --needed --noconfirm neovim yazi satty grim slurp
+elif command -v apt &>/dev/null; then
+  sudo apt install -y neovim
+elif command -v dnf &>/dev/null; then
+  sudo dnf install -y neovim
 fi
