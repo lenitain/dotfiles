dd4754df4dd2a58fd82f703eb8a8d26446d2fb11
pilot-whale <xt.zhu@qq.com>
2026-04-01 10:32:47 +0800

    bug fix: remove Chinese user-dirs

--- .chezmoiscripts/run_once_create-user-dirs.sh
+++ .chezmoiscripts/run_once_create-user-dirs.sh
@@ -9,3 +9,12 @@ mkdir -p ~/Documents
 mkdir -p ~/Music
 mkdir -p ~/Pictures
 mkdir -p ~/Videos
+
+rm -rf ~/桌面
+rm -rf ~/下载
+rm -rf ~/模板
+rm -rf ~/公共
+rm -rf ~/文档
+rm -rf ~/音乐
+rm -rf ~/图片
+rm -rf ~/视频
