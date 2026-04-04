0cf328a385a74384bb3b8d9cbd6d40ab0c09489e
lenitain <xt.zhu@qq.com>
2026-04-04 15:42:30 +0800

    add docker after installion

--- .chezmoiscripts/run_once_after_install-packages.sh
+++ .chezmoiscripts/run_once_after_install-packages.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if command -v pacman &>/dev/null; then
-  sudo pacman -S --needed --noconfirm neovim yazi satty grim slurp swayidle fcitx5 fcitx5-chinese-addons fcitx5-configtool lazygit cliphist htop strace
+  sudo pacman -S --needed --noconfirm neovim yazi satty grim slurp swayidle fcitx5 fcitx5-chinese-addons fcitx5-configtool lazygit cliphist htop strace docker
 elif command -v apt &>/dev/null; then
   sudo apt install -y neovim
 elif command -v dnf &>/dev/null; then
