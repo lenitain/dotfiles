36c72eb77e357b4b82b16469c860886c77ef4e08
lenitain <xt.zhu@qq.com>
2026-04-09 08:20:59 +0800

    remove aubio

--- .chezmoiscripts/run_once_after_install-packages.sh
+++ .chezmoiscripts/run_once_after_install-packages.sh
@@ -10,9 +10,8 @@ if command -v pacman &>/dev/null; then
     fcitx5 fcitx5-chinese-addons fcitx5-configtool \
     lazygit cliphist \
     htop strace gdb \
-    aria2 imv \
+    aria2 imv mpv \
     hurl \
-    aubio mpv \
 elif command -v apt &>/dev/null; then
   sudo apt install -y neovim
 elif command -v dnf &>/dev/null; then
