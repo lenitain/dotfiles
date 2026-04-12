b991f5077cb6a27af7c4ccfe2ede5a563775eb69
lenitain <xt.zhu@qq.com>
2026-04-12 09:39:29 +0800

    fix install script bug

--- .chezmoiscripts/run_once_after_install-packages.sh
+++ .chezmoiscripts/run_once_after_install-packages.sh
@@ -1,19 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
-if command -v pacman &>/dev/null; then
-  sudo pacman -S --needed --noconfirm \
-    neovim \
-    yazi \
-    satty grim slurp \
-    swayidle \
-    fcitx5 fcitx5-chinese-addons fcitx5-configtool \
-    lazygit cliphist \
-    htop strace gdb \
-    aria2 imv mpv \
-    hurl zip \
-elif command -v apt &>/dev/null; then
-  sudo apt install -y neovim
-elif command -v dnf &>/dev/null; then
-  sudo dnf install -y neovim
-fi
+sudo pacman -S --needed --noconfirm \
+  neovim \
+  yazi \
+  satty grim slurp \
+  swayidle \
+  fcitx5 fcitx5-chinese-addons fcitx5-configtool \
+  lazygit cliphist \
+  htop strace gdb \
+  aria2 imv mpv \
+  hurl zip
