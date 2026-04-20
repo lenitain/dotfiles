6ea85c42e2afeb5eb6d2a58505bece5e926e8ae0
lenitain <xt.zhu@qq.com>
2026-04-20 19:52:54 +0800

    feat(niri): optimize select logic

--- dot_config/niri/scripts/executable_translate.sh
+++ dot_config/niri/scripts/executable_translate.sh
@@ -2,18 +2,22 @@
 
 DIC_DB="$HOME/.local/share/stardict/ecdict.db"
 
-TEXT=$(wl-paste --primary)
+TEXT=$(wl-paste --primary | xargs)
 if [ -z "$TEXT" ]; then
   notify-send "Translate" "No text selected"
   exit 1
 fi
 
-TEXT_LOWER=$(echo "$TEXT" | tr '[:upper:]' '[:lower:]')
+RESULT=$(sqlite3 "$DIC_DB" "SELECT 
+  '音标: /' || COALESCE(phonetic, '') || '/' || char(10) ||
+  '中文: ' || COALESCE(translation, '') || char(10) ||
+  '英文: ' || COALESCE(definition, '') || char(10) ||
+  '变形: ' || COALESCE(exchange, '')
+FROM ecdict WHERE word='$TEXT' COLLATE NOCASE LIMIT 1;")
 
-RESULT=$(sqlite3 "$DIC_DB" "SELECT definition FROM ecdict WHERE word='$TEXT_LOWER' COLLATE NOCASE LIMIT 1;")
 if [ -z "$RESULT" ]; then
   RESULT="Not found: $TEXT"
 fi
 
-notify-send "$TEXT_LOWER: " "$RESULT"
+notify-send "$TEXT" "$RESULT"
 echo -n "$RESULT" | wl-copy
