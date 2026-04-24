e85d456f86f04ab5d186c53bbcdd89062bf3bca1
lenitain <xt.zhu@qq.com>
2026-04-24 17:33:32 +0800

    The `reafeat(nvim): set the indentation to 4 spaces for C/C++ files.

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "BufRead", "FileType" }, {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftround = true
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
