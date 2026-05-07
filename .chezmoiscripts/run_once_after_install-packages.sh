62c1953afc4ef5204536aa858a889fd96174d1e1
lenitain <xt.zhu@qq.com>
2026-05-07 21:10:04 +0800

    feat(fcitx5): add rime-ice configuration

--- .chezmoiscripts/run_once_after_install-packages.sh
+++ .chezmoiscripts/run_once_after_install-packages.sh
@@ -2,17 +2,14 @@
 set -euo pipefail
 
 sudo pacman -Syu --needed --noconfirm \
-  neovim \
-  yazi \
+  neovim yazi \
   satty grim slurp \
   swayidle \
-  fcitx5 fcitx5-chinese-addons fcitx5-configtool \
-  lazygit cliphist \
-  htop strace gdb \
+  fcitx5 fcitx5-chinese-addons fcitx5-configtool fcitx5-rime lazygit cliphist \
+  htop strace gdb bpftrace \
   aria2 imv mpv \
   hurl zip \
   kdenlive evemu \
   tesseract tesseract-data-eng \
-  bpftrace \
   zathura zathura-cb zathura-pdf-mupdf zathura-ps zathura-djvu \
   glow
