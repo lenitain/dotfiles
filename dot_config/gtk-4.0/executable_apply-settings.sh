8aa9d8cc0395a00361c458fc65f85c42fd8d12ee
lenitain <xt.zhu@qq.com>
2026-05-09 08:29:04 +0800

    feat(gtk): add everforest dark medium theme

--- dot_config/gtk-4.0/executable_apply-settings.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface font-name 'Inter 13'
-gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
